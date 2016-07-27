b096fe79842b56091f9812dafc2b24b09e6ece61
Shkoda <shkoshka@gmail.com>
2016-07-27 23:21:29 +0300

    123

--- GenFSharp/Program.fs
+++ GenFSharp/Program.fs
@@ -6,31 +6,38 @@ open System
 
 module ValueGenerator = 
     let randomGenerator = new System.Random()
+
     let randomInt = fun () -> randomGenerator.Next(0,1000)
-    let randomString = fun () -> "tmp"
-    let valueGenerators = dict[int.GetType(), randomInt; ]
-
- //   let res<'T> = fun (inputValue:obj) -> if (inputValue :? 'T) then inputValue :?> 'T else Unchecked.defaultof<'T>
- //   let castAs<'T> (o:obj) = 
- //       match o with
- //       | :? 'T as res -> res
- //       | _ -> Unchecked.defaultof<'T>
-
-   // let randomValue = fun () -> 
-  //  let inline genFunc<'T when 'T:> Type> ( ) =
-  //  let castAs<'T> = fun (inputValue:obj) -> if (inputValue :? 'T) then inputValue :?> 'T else Unchecked.defaultof<'T>  
+    let randomString = fun () -> "random_text_" + randomInt().ToString()
 
     let cast<'T> (inputValue : obj) = 
         match inputValue with
         | :? 'T as typedObject -> typedObject 
         | _ ->  Unchecked.defaultof<'T>  
    
-    let inline genFunc<'T> () =
+    let inline getRandomOfType<'T> () =
       match typeof<'T> with
-      | float -> 42.0 |> cast<'T>
-      | int -> randomInt() |> cast<'T>
+      | x when x = typeof<float>  -> 42.0 |> cast<'T>
+      | x when x = typeof<int>  -> randomInt() |> cast<'T>
       | _ ->  randomString() |> cast<'T>
 
+    let supportedTypes = [|float.GetType(); int.GetType(); string.GetType()|]
+    let randomType = fun() -> supportedTypes.[randomGenerator.Next(supportedTypes.Length)]
+    
+    type Option<'T> (value: 'T) =
+        member this.toString = String.Format("Option<{0}>", value)
+
+    type Tuple<'A, 'B> (first: 'A, second: 'B) =
+        member this.toString = String.Format("Tuple<{0}, {1}>", first, second)
+   
+    type wrapperEnum = Option | Tuple
+
+
+    let generify = fun (times : int) ->
+        let wrappers = Enum.GetValues(typeof<wrapperEnum>)
+        let length = wrappers.Length
+        let randWrapperType = wrappers(randomGenerator.Next(length))
+     
 
 
 
@@ -38,7 +45,7 @@ module ValueGenerator =
 [<EntryPoint>]
 let main argv = 
 
-    Console.WriteLine("random {0}", ValueGenerator.genFunc<float>())
+    Console.WriteLine("random {0}", ValueGenerator.getRandomOfType<string>())
     Console.ReadLine()
 
     0 // return an integer exit code
