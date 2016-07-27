c1bea073227b016c4281a141256da4db3b28a08f
Shkoda <shkoshka@gmail.com>
2016-07-27 18:38:04 +0300

    correct syntax

--- GenFSharp/Program.fs
+++ GenFSharp/Program.fs
@@ -9,11 +9,19 @@ module ValueGenerator =
     let randomInt = fun () -> randomGenerator.Next(0,1000)
     let randomString = fun () -> "tmp"
     let valueGenerators = dict[int.GetType(), randomInt; ]
+
+    let res<'T> = fun (inputValue:obj) -> if (inputValue :? 'T) then inputValue :?> 'T else Unchecked.defaultof<'T>
+    let castAs<'T> (o:obj) = 
+        match o with
+        | :? 'T as res -> res
+        | _ -> Unchecked.defaultof<'T>
+
    // let randomValue = fun () -> 
-    let inline genFunc<'T when 'T:> Type> (arg : 'T ) =
-      match arg with
-        | int -> randomInt()
-        | _ -> randomString()  
+  //  let inline genFunc<'T when 'T:> Type> ( ) =
+    let inline genFunc<'T> ( ) =
+      match typeof<'T>  with
+        | int -> castAs<'T>(randomInt() )
+        | _ ->  castAs<'T>(randomString() )
 
 
 
@@ -21,7 +29,7 @@ module ValueGenerator =
 [<EntryPoint>]
 let main argv = 
 
-    Console.WriteLine("random {0}", ValueGenerator.genFunc(int.GetType()))
+    Console.WriteLine("random {0}", ValueGenerator.genFunc<string>())
     Console.ReadLine()
 
     0 // return an integer exit code
