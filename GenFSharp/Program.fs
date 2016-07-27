052c55256287db558400f9a6037146a936b93543
Shkoda <shkoshka@gmail.com>
2016-07-27 18:24:50 +0300

    aaaaa

--- GenFSharp/Program.fs
+++ GenFSharp/Program.fs
@@ -7,16 +7,21 @@ open System
 module ValueGenerator = 
     let randomGenerator = new System.Random()
     let randomInt = fun () -> randomGenerator.Next(0,1000)
-    let types = dict[int.GetType(), randomInt]
-
-
+    let randomString = fun () -> "tmp"
+    let valueGenerators = dict[int.GetType(), randomInt; ]
+   // let randomValue = fun () -> 
+    let inline genFunc<'T when 'T:> Type> (arg : 'T ) =
+      match arg with
+        | int -> randomInt()
+        | _ -> randomString()  
 
 
 
 
 [<EntryPoint>]
 let main argv = 
-    Console.WriteLine("random {0}", ValueGenerator.randomInt())
+
+    Console.WriteLine("random {0}", ValueGenerator.genFunc(int.GetType()))
     Console.ReadLine()
 
     0 // return an integer exit code
