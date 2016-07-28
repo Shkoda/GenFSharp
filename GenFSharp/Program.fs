a9aea2bedd40680302018c5e995033bd063048e5
Shkoda <shkoshka@gmail.com>
2016-07-28 12:44:35 +0300

    prototype works

--- GenFSharp/Program.fs
+++ GenFSharp/Program.fs
@@ -25,27 +25,47 @@ module ValueGenerator =
     let randomType = fun() -> supportedTypes.[randomGenerator.Next(supportedTypes.Length)]
     
     type Option<'T> (value: 'T) =
-        member this.toString = String.Format("Option<{0}>", value)
+        member this.asString = String.Format("Option<{0}>", value)
 
     type Tuple<'A, 'B> (first: 'A, second: 'B) =
-        member this.toString = String.Format("Tuple<{0}, {1}>", first, second)
+        member this.asString = String.Format("Tuple<{0}, {1}>", first, second)
    
-    type wrapperEnum = Option | Tuple
 
+    type ValueOrError<'A> (value: 'A) =
+        member this.asString = String.Format("ValueOrError<{0}>", value)
+   
+    type WrapperEnum = 
+        |Option = 0
+        |Tuple = 1
+        |ValueOrError = 2
+
+    
+    let randomWrapper = fun () -> enum<WrapperEnum>(randomGenerator.Next(Enum.GetValues(typeof<WrapperEnum>).Length))
 
     let generify = fun (times : int) ->
-        let wrappers = Enum.GetValues(typeof<wrapperEnum>)
-        let length = wrappers.Length
-        let randWrapperType = wrappers(randomGenerator.Next(length))
+           
+        let wrap = fun accum ->
+            match randomWrapper() with
+             | WrapperEnum.Option ->(new Option<string>(accum)).asString
+             | WrapperEnum.Tuple ->(new Tuple<string, int>(accum, getRandomOfType<int>())).asString
+             | _ -> accum
+
+        let rec recWrap accum count = 
+            match count with
+            | 0 -> accum
+            | _ -> recWrap(wrap(accum))(count-1)
+
+        let someValue = getRandomOfType<string>()
+        recWrap someValue times
+  
+
      
-
-
 
 
 [<EntryPoint>]
 let main argv = 
-
-    Console.WriteLine("random {0}", ValueGenerator.getRandomOfType<string>())
+    let text = ValueGenerator.generify(5)
+    Console.WriteLine(text)
     Console.ReadLine()
 
     0 // return an integer exit code
